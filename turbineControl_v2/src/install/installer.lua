06640e8c64117d97ca3e524a7cbb2e0b97d990c9
Sauerkrautonaut <Quentyn.pfeuffer@protonmail.com>
2024-05-12 18:50:34 +0200

    Update installer.lua to use files for fork

--- turbineControl_v2/src/install/installer.lua
+++ turbineControl_v2/src/install/installer.lua
@@ -39,7 +39,7 @@ else
 end
 
 --Url for file downloads
-local relUrl = "https://raw.githubusercontent.com/ThorsCrafter/Reactor-and-Turbine-control-program/"..branch.."/turbineControl_v2/src/"
+local relUrl = "https://raw.githubusercontent.com/Sauerkrautonaut/Reactor-and-Turbine-control-program/"..branch.."/turbineControl_v2/src/"
 
 
 --===== Functions =====
